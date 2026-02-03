61b7f98c9aefb40579e82e12318d1db66f07563d
daniilPROBAS <151975794+daniilanxieplayaqwerty@users.noreply.github.com>
2026-02-03 22:39:32 +0200

    Add files via upload

--- js/app/widgets/msg.js
+++ js/app/widgets/msg.js
@@ -77,6 +77,32 @@ export const msg = {
                 }, 3000);
 
             }, 100);
+        },
+
+        confirmFun(title, text) {
+            this.code = 0;
+            var self = this;
+
+            return new Promise(function (resolve, reject) {
+                self.confirmTitle = title;
+                self.confirm = text;
+                self.$refs.confirm.active = 1;
+
+                self.interval = setInterval(function () {
+                    if (self.code > 0) resolve();
+                }, 100);
+            }).then(function () {
+                clearInterval(self.interval);
+                self.$refs.confirm.active = 0;
+
+                if (self.code == 1) {
+                    return true;
+                }
+
+                if (self.code == 2) {
+                    return false;
+                }
+            });
         }
     },
 
@@ -94,5 +120,17 @@ export const msg = {
                 </div>
             </div>
         </div>
+
+        <popup ref="confirm" :title="confirmTitle">
+            <div class="al">
+                <i class="fas fa-info-circle"></i> {{ confirm }}
+
+                <div class="botBtns">
+                    <a class="btnS" href="#" @click.prevent="code = 1">Yes</a>
+                    <a class="btnS" href="#" @click.prevent="code = 2">No</a>
+                </div>
+            </div>
+        </popup>
+
     `
 };
